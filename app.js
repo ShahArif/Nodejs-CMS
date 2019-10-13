587d764544843881ba0963de2b270c0462bc4ae4
ariflateefshah <arif@ideas2it.com>
2019-10-13 18:15:51 +0530

    Feature: Configurations | MongoDb Connection

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+// Configure to connect mongooDb
+mongoose.connect('mongodb://localhost:27017/nodejs_cms',{ useNewUrlParser:true })
+.then(response =>{
+  console.log('mongoDB COnnected Successfully');
+}).catch((err) => {
+    console.log('Database Connection error');
+})
+
+
+
+// Configure express
+ app.use(express.json());
+ app.use(express.urlencoded({extended:true}));
+ app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+
+app.use('/', (req , res) =>{
+  res.send('Welcome to new CMS');
+});
+
+
+app.listen(3002 , () =>{
+  console.log('server running on port 3002');
+});
